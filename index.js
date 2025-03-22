4af2bcc28c458a22acc396addec85409287ddaec
robertkutwa <robertkutwa0@gmail.com>
2025-03-22 13:40:51 +0300

    Destructuring Assignment (CodeGrade)

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+// const farmAnimals = 'cow horse sheep pig chicken';
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
-};
+// const muppet = {
+//   muppetName: 'Miss Piggy',
+//   color: 'pink',
+//   song: 'Never Before, Never Again',
+//   job: 'Cast member of The Muppet Show',
+//   partner: 'Kermit'
+// };
 
-const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
-};
+// const nestedMuppet = {
+//   nestedName: 'Kermit',
+//   nestedColor: 'green',
+//   album: {
+//     theMuppetMovie: {
+//       song1: 'Rainbow Connection',
+//       song2: 'Moving Right Along',
+//       song3: 'Never Before, Never Again',
+//       song4: 'I Hope That Something Better Comes Along',
+//     },
+//   },
+//   nestedJob: 'Host of The Muppet Show',
+//   nestedPartner: 'Miss Piggy'
+// };
 
 // Strings
 
@@ -46,3 +46,50 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+
+const farmAnimals = 'cow horse sheep pig chicken';
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg, ] = colors;
+
+const muppet = {
+  muppetName: 'Miss Piggy',
+  color: 'pink',
+  song: 'Never Before, Never Again',
+  job: 'Cast member of The Muppet Show',
+  partner: 'Kermit'
+};
+
+const { muppetName, color, song, job, partner } = muppet;
+
+const nestedMuppet = {
+  nestedName: 'Kermit',
+  nestedColor: 'green',
+  album: {
+    theMuppetMovie: {
+      song1: 'Rainbow Connection',
+      song2: 'Moving Right Along',
+      song3: 'Never Before, Never Again',
+      song4: 'I Hope That Something Better Comes Along',
+    },
+  },
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner: 'Miss Piggy'
+};
+
+const {
+  album: {
+    theMuppetMovie: { song2, song4 }
+  },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
+
